a1f22c866173128e7f41073bc9bb57590175e662
unknown <you@example.com>
2020-06-30 17:41:08 -0700

    Initial commit

---
+++ Scraps.ps1
@@ -0,0 +1,12 @@
+﻿$var = Get-Clipboard
+
+while ($true -eq 2)
+{  
+    $var2 = Get-Clipboard
+    if ($var -ne $var2){   
+        #The filepath needs to be changed to something on your local computer  
+        get-clipboard | Out-file -FilePath  C:\filepath\on\your\computer.txt -append 
+        $var = Get-Clipboard
+        $var2 = $var
+    }   
+}
